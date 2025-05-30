aedff6ee43d52482baf5b55a874f750b4daea797
Wilker Lopes <153626335+WilkerJoseLopes@users.noreply.github.com>
2025-05-30 09:30:35 +0100

    Update __init__.py

--- Gestao_de_Consumo/__init__.py
+++ Gestao_de_Consumo/__init__.py
@@ -1,1 +1,7 @@
-from Gestao_de_Consumo import *
+from .main import ControladorCasas
+
+def main():
+    sistema = ControladorCasas()
+    # Exemplo de uso básico:
+    sistema.adicionar_casa("001", 41.1578, -8.6299, "Rua do Porto 123", "A+", "Casa Central")
+    sistema.gerar_relatorio_geral()
